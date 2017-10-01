1bdb09db5505403cd03536535d247feaa5c1cfa4
Simon Fish <si@mon.fish>
2017-10-01 16:46:14 +0100

    Implement scoreBoard

--- src/domino.hs
+++ src/domino.hs
@@ -16,7 +16,23 @@ knockingP h b = if null h
                             else (if goesP (head h) (last b) b
                                     then False
                                     else knockingP (tail h) b))
-possPlays :: Hand -> Board -> Domino
-possPlays h b = if null h
-                   then "end"
-                   else goesP (head b) 
+-- PSEUDOCODE (ideas) for functions to come
+-- possPlays :: Hand -> Board -> Domino
+-- possPlays h b = if null h
+--                    then Null
+--                    else (if null b
+--                             then h
+--                             else (0,0))
+-- RETURN TYPE SHOULD BE A PAIR
+-- playDom :: Domino -> Board -> End -> Board
+-- playDom d b e = if goesP d e b
+--                    then (if e == (head b)
+--                             then -- add to start of board
+--                             else -- add to end of board)
+-- RETURN TYPE SHOULD BE A MAYBE
+scoreBoard :: Board -> Int
+scoreBoard b = if mod (fst (head b) + snd (last b)) 3 == 0
+                  then quot ((fst (head b)) + (snd (last b))) 3
+                  else (if mod (fst (head b) + snd (last b)) 5 == 0
+                           then quot ((fst (head b)) + (snd (last b))) 5
+                           else 0)
