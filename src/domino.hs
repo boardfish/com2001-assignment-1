25e68353f59d24032815beff0b9af5b10098755f
Simon Fish <si@mon.fish>
2017-10-02 15:31:22 +0100

    Add ScoreN function

--- src/domino.hs
+++ src/domino.hs
@@ -11,19 +11,11 @@ goesP d e b = if e == (last b)
 knockingP :: Hand -> Board -> Bool
 knockingP h b = if null h
                    then True
-                   else (if goesP (head h) (head b) b
+                   else (if goesP (head h) (head b) b || 
+                            goesP (head h) (last b) b
                             then False
-                            else (if goesP (head h) (last b) b
-                                    then False
-                                    else knockingP (tail h) b))
+                            else knockingP (tail h) b)
 -- PSEUDOCODE (ideas) for functions to come
--- possPlays :: Hand -> Board -> Domino
--- possPlays h b = if null h
---                    then Null
---                    else (if null b
---                             then h
---                             else (0,0))
--- RETURN TYPE SHOULD BE A PAIR
 -- playDom :: Domino -> Board -> End -> Board
 -- playDom d b e = if goesP d e b
 --                    then (if e == (head b)
@@ -36,3 +28,10 @@ scoreBoard b = if mod (fst (head b) + snd (last b)) 3 == 0
                   else (if mod (fst (head b) + snd (last b)) 5 == 0
                            then quot ((fst (head b)) + (snd (last b))) 5
                            else 0)
+-- possPlays :: Hand -> Board -> Domino
+-- possPlays h b = if goesP (head h) (head b) b || goesP (head h) (last b) b
+--                    then head h
+--                    else null
+-- RETURN TYPE SHOULD BE A PAIR
+scoreN :: Board -> Int -> [Domino]
+scoreN b s = [(x,y) | x <- [0..6], y<- [0..6], not(elem (x,y) b) && mod (x+y) s == 0]
