865f2bc0fc8fdf194d3fb2738095ec25ea59ad89
Simon Fish <si@mon.fish>
2017-11-28 10:16:08 +0000

    Add countHandSpots

--- src/DominoesIntel.hs
+++ src/DominoesIntel.hs
@@ -13,6 +13,8 @@ module DominoesIntel where
     countSpots _ [] = []
     countSpots 7 l = l
     countSpots y l = count y l : countSpots (y+1) (deleteAllInstances y l)
+    countHandSpots :: Hand -> [Int]
+    countHandSpots h = countSpots 0 (handToSpots h)
 
 -- For each item in the list,
 --     check its number and
